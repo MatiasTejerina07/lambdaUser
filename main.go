b8e5930ec1d998ee32401fb43f51978a639a0e03
Mat <matiastejerina94@gmail.com>
2023-10-20 23:44:28 -0300

    db/common

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"lambdaUser/aws"
+	"lambdaUser/db"
+	"lambdaUser/models"
+	"os"
 
 	event "github.com/aws/aws-lambda-go/events"
 	lambda "github.com/aws/aws-lambda-go/lambda"
@@ -15,4 +20,35 @@ func main() {
 func RunLambdaUser(ctx context.Context, event event.CognitoEventUserPoolsPostConfirmation) (event.CognitoEventUserPoolsPostConfirmation, error) {
 	aws.InitialAws()
 
+	if !ValidateParameters() {
+		fmt.Println("Error en los parámetros. debe enviar 'SecretName'")
+		err := errors.New("Error en los parámetros. debe enviar 'SecretName'")
+		return event, err
+	}
+	var datos models.SignUp
+
+	for row, att := range event.Request.UserAttributes {
+		switch row {
+		case "email":
+			datos.UserEmail = att
+			fmt.Print("Email ==" + datos.UserEmail)
+		case "sub":
+			datos.UserUUID = att
+			fmt.Print("Email ==" + datos.UserUUID)
+		}
+	}
+
+	err := db.ReadSecret()
+
+	if err != nil {
+		fmt.Println("Error al leer el secret" + err.Error())
+		return event, err
+	}
+
+}
+
+func ValidateParameters() bool {
+	var Parameters bool
+	_, Parameters = os.LookupEnv("SecretName")
+	return Parameters
 }
